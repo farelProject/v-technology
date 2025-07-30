f5952005307e052b40162fa48597783b9e570538
Farel Developer <farproject.studio@gmail.com>
2025-07-30 15:10:40 +0000

    Tambahkan tombol history.back() pada halaman login

--- src/components/auth-layout.tsx
+++ src/components/auth-layout.tsx
@@ -1,5 +1,11 @@
+
+'use client';
+
 import Link from 'next/link';
 import { VTechIcon } from './vtech-icon';
+import { Button } from './ui/button';
+import { ArrowLeft } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 
 export function AuthLayout({
   title,
@@ -10,8 +16,18 @@ export function AuthLayout({
   description: string;
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+
   return (
     <div className="container relative flex h-screen flex-col items-center justify-center">
+       <Button
+        variant="ghost"
+        className="absolute left-4 top-4 md:left-8 md:top-8"
+        onClick={() => router.back()}
+      >
+        <ArrowLeft className="mr-2 h-4 w-4" />
+        Back
+      </Button>
       <div className="lg:p-8">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
